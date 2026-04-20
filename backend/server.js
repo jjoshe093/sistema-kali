c16162e5738eea656d1c2006f38f530daed3048f
Psmt 6704 <jjoshe093@gmail.com>
2026-04-20 02:10:26 -0600

    se agregan los archivos necesarios

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const cors = require('cors'); // Asegúrate de haber hecho: npm install cors
 const app = express();
 
+app.use(cors()); // Esto le dice al backend: "Acepta peticiones de cualquier sitio"
 app.use(express.json());
 
 // Registrar una venta
